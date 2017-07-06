5a0ce68ffb83112d87535c26a06a3dd352fadafc
Connor Heidema <cheidema@sfu.ca>
2017-07-06 16:41:04 -0700

    Learned how to instantiate sprites at runtime. Plan to develop title screen

---
+++ UnityRPG/Assets/Scripts/Begin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Begin : MonoBehaviour {
+     public Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+         GameObject go = new GameObject("Load Game Select");
+         SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
+         sprite = Resources.Load<Sprite>("battleAttack");
+         renderer.sprite = sprite;
+	}
+	
+	// Update is called once per frame
+ void Update()
+ {
+
+ }
+}
