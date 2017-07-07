5215eb32642055ade15fe05fa98627b68b9991fd
Connor Heidema <cheidema@sfu.ca>
2017-07-06 17:13:32 -0700

    Added jiggling buttons to indicate a selection potentially

--- UnityRPG/Assets/Scripts/Begin.cs
+++ UnityRPG/Assets/Scripts/Begin.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Begin : MonoBehaviour {
-     public Sprite sprite;
-
+    public Sprite sprite;
+    GameObject go;
 	// Use this for initialization
 	void Start () {
-         GameObject go = new GameObject("Load Game Select");
-         SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
-         sprite = Resources.Load<Sprite>("battleAttack");
-         renderer.sprite = sprite;
+	
 	}
 	
 	// Update is called once per frame
- void Update()
- {
+	void FixedUpdate()
+	{
+		go = new GameObject("Load Game Select");
+	    SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
 
- }
+	    renderer.sprite = sprite;
+	    if (Time.realtimeSinceStartup > 5){
+	    	sprite = Resources.Load<Sprite>("battleDefend");
+	    }
+	    else{
+	    	sprite = Resources.Load<Sprite>("battleAttack");
+	    }
+	    go.transform.localScale = new Vector2(10,10);
+	    go.transform.localEulerAngles = new Vector3(0,0,10 *(float)System.Math.Sin(Time.realtimeSinceStartup*10));
+	    renderer.sprite = sprite;
+		Destroy(go,0.02f);
+	}
 }
