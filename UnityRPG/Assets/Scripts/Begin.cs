a54cc6c6f91983c1839831ee78b8aad8266c2460
Connor Heidema <cheidema@sfu.ca>
2017-07-12 15:33:19 -0700

    Basic Button action with keys

--- UnityRPG/Assets/Scripts/Begin.cs
+++ UnityRPG/Assets/Scripts/Begin.cs
@@ -4,25 +4,52 @@ using UnityEngine;
 public class Begin : MonoBehaviour {
 	GameObject newGame;
 	GameObject continueGame;
+	GameMode myGameMode;
+	ControlMode myControlMode;
+	int menu;
 	// Use this for initialization
 	void Start () {
-		GameMode myGameMode = GameMode.TitleScreen;
-		ControlMode myControlMode = ControlMode.LoadNew;
+		myGameMode = GameMode.TitleScreen;
+		myControlMode = ControlMode.Nothing;
+		menu = 0;
 	}
+	   bool GUIKeyDown(KeyCode key)
+    {
+        if (Event.current.type == EventType.KeyDown)
+            return (Event.current.keyCode == key);
+        return false;
+    }
 	// Update is called once per frame
-	void FixedUpdate()
+	void Update()
 	{
-		newGame = new GameObject("Load Game Select");
-	    SpriteRenderer renderer = newGame.AddComponent<SpriteRenderer>();
-	    if (Time.realtimeSinceStartup > 5){
-			renderer.sprite = Resources.Load<Sprite>("battleDefend");
-	    }
-	    else{
-			renderer.sprite = Resources.Load<Sprite>("battleAttack");
-	    }
-	    newGame.transform.localScale = new Vector2(10,10);
-	    newGame.transform.localEulerAngles = new Vector3(0,0,10 *(float)System.Math.Sin(Time.realtimeSinceStartup*10));
-		Destroy(newGame,0.02f);
+		if (myControlMode == ControlMode.Nothing){
+			if (Input.GetKeyDown(KeyCode.W))
+   			{
+   				menu = 1;
+        	}
+        	if (Input.GetKeyDown(KeyCode.S)){
+        		menu = 0;
+        	}
+        	if (menu == 1 && Input.GetKeyDown(KeyCode.Space)){
+        		myControlMode = ControlMode.Walk;
+        	}
+        	if (menu == 0 && Input.GetKeyDown(KeyCode.Space)){
+        		myControlMode = ControlMode.MiniGame;
+        	}
+		}
+		if (myControlMode == ControlMode.Nothing){
+			newGame = new GameObject("Load Game Select");
+		    SpriteRenderer renderer = newGame.AddComponent<SpriteRenderer>();
+		    if (menu == 1){
+					renderer.sprite = Resources.Load<Sprite>("battleAttack");
+		    }
+		    else if (menu == 0){
+	        		renderer.sprite = Resources.Load<Sprite>("battleDefend");
+		    }
+		    newGame.transform.localScale = new Vector2(10,10);
+		    newGame.transform.localEulerAngles = new Vector3(0,0,10 *(float)System.Math.Sin(Time.realtimeSinceStartup*10));
+			Destroy(newGame,0.02f);
+		}
 	}
 
 	void RunGame(GameMode myGameMode, ControlMode myControlMode){
